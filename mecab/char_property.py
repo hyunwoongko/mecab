fd196230b18c1aba0df36f66dbf9b65274f85a41
Hyunwoong Ko <gusdnd852@naver.com>
2021-01-30 00:29:19 +0900

    add char property

--- mecab/char_property.py
+++ mecab/char_property.py
@@ -3,6 +3,8 @@
 # Copyright(C) 2004-2006 Nippon Telegraph and Telephone Corporation
 # Copyright(C) 2021 Hyunwoong Ko <kevin.woong@kakaobrain.com>
 
+from typing import List, Dict
+
 from mecab.utils import CHECK_DIE
 
 
@@ -14,11 +16,11 @@ class CharInfo:
         https://github.com/taku910/mecab/blob/master/mecab/src/char_property.h#L16
     """
 
-    _type: int = 0
-    _default_type: int = 0
-    _length: int = 0
-    _group: int = 0
-    _invoke: int = 0
+    type: int = 0
+    default_type: int = 0
+    length: int = 0
+    group: int = 0
+    invoke: int = 0
 
     def is_kind_of(self, c) -> int:
         """
@@ -31,7 +33,7 @@ class CharInfo:
             (int): result of bitwise AND operation
         """
 
-        return self._type & c._type
+        return self.type & c.type
 
 
 class Range:
@@ -42,9 +44,45 @@ class Range:
         https://github.com/taku910/mecab/blob/master/mecab/src/char_property.cpp#L20
     """
 
-    _low: int
-    _high: int
-    _c: list
+    low: int
+    high: int
+    c: list
+
+
+class CharProperty:
+
+    def open_param(self, param) -> bool:
+        pass
+
+    def open_char(self, char) -> bool:
+        pass
+
+    def close(self):
+        pass
+
+    def size(self):
+        pass
+
+    def set_charset(self, charset):
+        pass
+
+    def id(self, char):
+        pass
+
+    def name(self, i):
+        pass
+
+    def what(self):
+        pass
+
+    def seek_to_other_type(self, begin, end, c, fail, mblen, clen):
+        pass
+
+    def get_char_info(self, begin, end, mblen):
+        pass
+
+    def compile(self):
+        pass
 
 
 def atohex(s: str) -> int:
@@ -57,15 +95,11 @@ def atohex(s: str) -> int:
     Returns:
         (int) integer value
 
-    Notes:
-        I can't understand why the name of this function is 'atohex'
-        because output of function is decimal, not hexadecimal.
-
     Examples:
         >>> atohex('0x10')
         16
         >>> atohex('hello')
-        no hex value: hello
+        'no hex value: hello'
 
     References:
         https://github.com/taku910/mecab/blob/master/mecab/src/char_property.cpp#L26
@@ -79,5 +113,36 @@ def atohex(s: str) -> int:
     return int(s, 0)
 
 
+def encode(c: List[str], category: Dict[str, CharInfo]) -> CharInfo:
+    """
+    encode from list of strings
 
+    Args:
+        c (List[str]): list of strings
+        category (Dict[str, CharInfo]): categories
 
+    Returns:
+        (CharInfo): encoded output charinfo
+    """
+
+    CHECK_DIE(
+        condition=len(c) != 0,
+        message="category size is empty"
+    )
+
+    CHECK_DIE(
+        condition=c[0] in category,
+        message=f'category [{c[0]}] is undefined',
+    )
+
+    base: CharInfo = category[c[0]]
+
+    for i in range(0, len(c)):
+        CHECK_DIE(
+            condition=c[i] in category,
+            message=f'category [{c[i]}] is undefined',
+        )
+
+        base.type += (1 << category[c[i]].default_type)
+
+    return base
