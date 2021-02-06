7e8edf5f9578aa0524f4f3bbbacd6df42517cd4b
Hyunwoong Ko <gusdnd852@naver.com>
2021-02-07 01:45:40 +0900

    Add data_structure.h

--- mecab/data_structure.py
+++ mecab/data_structure.py
@@ -1,4 +1,4 @@
-class Node:
+class NodeType:
     # Normal node defined in the dictionary.
     MECAB_NOR_NODE = 0
 
@@ -15,7 +15,7 @@ class Node:
     MECAB_EON_NODE = 4
 
 
-class Dict:
+class DictType:
     # This is a system dictionary.
     MECAB_SYS_DIC = 0
 
@@ -56,3 +56,145 @@ class RequestType:
     MECAB_ALLOCATE_SENTENCE = 64
 
 
+class BoundaryConstraintType:
+    # The token boundary is not specified.
+    MECAB_ANY_BOUNDARY = 0
+
+    # The position is a strong token boundary.
+    MECAB_TOKEN_BOUNDARY = 1
+
+    # The position is not a token boundary.
+    MECAB_INSIDE_TOKEN = 2
+
+
+class DictionaryInfo:
+    # filename of dictionary
+    # On Windows, filename is stored in UTF-8 encoding
+    filename: str
+
+    # character set of the dictionary. e.g., "SHIFT-JIS", "UTF-8"
+    charset: str
+
+    # How many words are registered in this dictionary.
+    size: int
+
+    # dictionary type
+    # this value should be MECAB_USR_DIC, MECAB_SYS_DIC, or MECAB_UNK_DIC.
+    type: int
+
+    # left attributes size
+    lsize: int
+
+    # right attributes soze
+    rsize: int
+
+    # version of this dictionary
+    version: int
+
+    # pointer to the next dictionary info.
+    # python can't access itself. (type of next is DictionaryInfo*)
+    next: object
+
+
+class Path:
+    # pointer to the right node
+    # type of rnode is Node*
+    rnode: object
+
+    # pointer to the next right path
+    # python can't access itself. (type of rnext is Path*)
+    rnext: object
+
+    # pointer to the left node
+    # type of lnode is Node*
+    lnode: object
+
+    # pointer to the next left path
+    # python can't access itself. (type of lnext is Path*)
+    lnext: object
+
+    # local cost
+    cost: int
+
+    # marginal probability
+    prob: float
+
+
+class Node:
+    # pointer to the previous node.
+    # python can't access itself. (type of prev is Node*)
+    prev: object
+
+    # pointer to the next node.
+    # python can't access itself. (type of next is Node*)
+    next: object
+
+    # pointer to the node which ends at the same position.
+    # python can't access itself. (type of enext is Node*)
+    enext: object
+
+    # pointer to the node which starts at the same position.
+    # python can't access itself. (type of bnext is Node*)
+    bnext: object
+
+    # pointer to the right path.
+    # this value is NULL if MECAB_ONE_BEST mode.
+    rpath: Path
+
+    # pointer to the left path.
+    # this value is NULL if MECAB_ONE_BEST mode.
+    lpath: Path
+
+    # surface string.
+    # this value is not 0 terminated.
+    # You can get the length with length/rlength members.
+    surface: str
+
+    # feature string
+    feature: str
+
+    # unique node id
+    id: int
+
+    # length of the surface form.
+    length: int
+
+    # length of the surface form including white space before the morph.
+    rlength: int
+
+    # right attribute id
+    rcAttr: int
+
+    # left attribute id
+    lcAttr: int
+
+    # unique part of speech id. This value is defined in "pos.def" file.
+    posid: int
+
+    # character type
+    char_type: int
+
+    # status of this model.
+    # This value is MECAB_NOR_NODE, MECAB_UNK_NODE, MECAB_BOS_NODE, MECAB_EOS_NODE, or MECAB_EON_NODE.
+    stat: int
+
+    # set 1 if this node is best node.
+    isbest: int
+
+    # forward accumulative log summation.
+    # This value is only available when MECAB_MARGINAL_PROB is passed.
+    alpha: float
+
+    # backward accumulative log summation.
+    # This value is only available when MECAB_MARGINAL_PROB is passed.
+    beta: float
+
+    # marginal probability.
+    # This value is only available when MECAB_MARGINAL_PROB is passed.
+    prob: float
+
+    # word cost
+    wcost: int
+
+    # best accumulative cost from bos node to this node.
+    cost: int
