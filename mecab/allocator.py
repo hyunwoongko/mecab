daacc694ba0d3e002c07cb0e8300ee2e4929fde6
Mineru <rmstjr1030@naver.com>
2021-05-16 23:28:45 +0900

    Fix Private Var

--- mecab/allocator.py
+++ mecab/allocator.py
@@ -4,21 +4,21 @@ from mecab.utils.scoped_ptr import ScopedPtr, ScopedArray
 from mecab.utils.freelist import FreeList, ChunkFreeList
 
 class Allocator:
-    __kResultsSize = 512
-    __partial_buffer_ = []
+    _kResultsSize = 512
+    _partial_buffer_ = []
 
     def __init__(self, typeN: type, typeP: type):
-        self.__type_n = typeN
-        self.__type_p = typeP
+        self._type_n = typeN
+        self._type_p = typeP
 
         # member
-        self.__id_ = 0
-        self.__node_freelist_ = ScopedPtr(FreeList(type_=typeN, size=NODE_FREELIST_SIZE))
-        self.__path_freelist_ = ScopedPtr(FreeList(type_=typeP, size=0))
-        self.__char_freelist_ = ScopedPtr(ChunkFreeList(type_=str, size=0))
-        self.__nbest_generator_ = ScopedPtr(NBestGenerator())
-        self.__results_ = ScopedArray(type_=str, size=self.__kResultsSize)
-        # self.__results_ = ScopedArray()
+        self._id_ = 0
+        self._node_freelist_ = ScopedPtr(FreeList(type_=typeN, size=NODE_FREELIST_SIZE))
+        self._path_freelist_ = ScopedPtr(FreeList(type_=typeP, size=0))
+        self._char_freelist_ = ScopedPtr(ChunkFreeList(type_=str, size=0))
+        self._nbest_generator_ = ScopedPtr(NBestGenerator())
+        self._results_ = ScopedArray(type_=str, size=self._kResultsSize)
+        # self._results_ = ScopedArray()
 
     def newNode(self):
         """[summary]
@@ -28,9 +28,9 @@ class Allocator:
         Returns:
             [typeN]: [description]
         """
-        self.__node_freelist_.data.alloc()
-        self.__id_ = self.__id_ + 1
-        return self.__node_freelist_.data
+        self._node_freelist_.data.alloc()
+        self._id_ = self._id_ + 1
+        return self._node_freelist_.data
 
     def newPath(self):
         """[summary]
@@ -39,10 +39,10 @@ class Allocator:
         Returns:
             [typeP]: [description]
         """
-        if self.__path_freelist_.get() != None:
-            self.__path_freelist_.reset(FreeList(type_=self.__type_p, size=PATH_FREELIST_SIZE))
-        self.__path_freelist_.data.alloc()
-        return self.__path_freelist_.data
+        if self._path_freelist_.get() != None:
+            self._path_freelist_.reset(FreeList(type_=self._type_p, size=PATH_FREELIST_SIZE))
+        self._path_freelist_.data.alloc()
+        return self._path_freelist_.data
 
     def mutable_results(self) -> list:
         """[summary]
@@ -50,13 +50,13 @@ class Allocator:
         Returns:
             [list]: [description]
         """
-        return self.__results_.get()
+        return self._results_.get()
 
     def alloc(self, size: int) -> list:
-        if self.__char_freelist_.get() != None:
-            self.__char_freelist_.reset(ChunkFreeList(type_=str, size=BUF_SIZE))
-        self.__char_freelist_.data.alloc(size + 1)
-        return self.__char_freelist_.data
+        if self._char_freelist_.get() != None:
+            self._char_freelist_.reset(ChunkFreeList(type_=str, size=BUF_SIZE))
+        self._char_freelist_.data.alloc(size + 1)
+        return self._char_freelist_.data
     
     # python에서는 size의 의미가 필요한가 싶네요.
     def strdup(self, str_: str, size: int) -> str:
@@ -64,39 +64,39 @@ class Allocator:
         return n_str
     
     def nbest_generator(self) -> NBestGenerator:
-        if self.__nbest_generator_.get() != None:
-            self.__nbest_generator_.reset(NBestGenerator())
-        return self.__nbest_generator_.get()
+        if self._nbest_generator_.get() != None:
+            self._nbest_generator_.reset(NBestGenerator())
+        return self._nbest_generator_.get()
     
     def partial_buffer(self, size: int) -> str:
-        if len(self.__partial_buffer_) == 0:
-            self.__partial_buffer_ = ['' for _ in range(size)]
+        if len(self._partial_buffer_) == 0:
+            self._partial_buffer_ = ['' for _ in range(size)]
         else:
-            self.__partial_buffer_ = self.__partial_buffer_[:(size + 1)]
-        return self.__partial_buffer_[0]
+            self._partial_buffer_ = self._partial_buffer_[:(size + 1)]
+        return self._partial_buffer_[0]
     
     def results_size(self) -> int:
-        return self.__kResultsSize
+        return self._kResultsSize
 
     def free(self):
-        self.__id_ = 0
-        self.__node_freelist_.data.free()
-        if self.__path_freelist_.get() != None:
-            # self.__path_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
-            self.__path_freelist_.data.free()
-        if self.__char_freelist_.get() != None:
-            # self.__char_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
-            self.__char_freelist_.data.free()
+        self._id_ = 0
+        self._node_freelist_.data.free()
+        if self._path_freelist_.get() != None:
+            # self._path_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
+            self._path_freelist_.data.free()
+        if self._char_freelist_.get() != None:
+            # self._char_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
+            self._char_freelist_.data.free()
     
     # only debugging
     def print_node_freelist(self):
-        print(self.__node_freelist_.data)
+        print(self._node_freelist_.data)
     
     def print_path_freelist(self):
-        print(self.__path_freelist_.data)
+        print(self._path_freelist_.data)
     
     def print_char_freelist(self):
-        print(self.__char_freelist_.data)
+        print(self._char_freelist_.data)
     
     def print_id(self):
-        print(self.__id_)
+        print(self._id_)
