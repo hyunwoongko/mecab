9d4875ac84516e700fdfba97e78f6016d94dec14
Mineru <rmstjr1030@naver.com>
2021-05-16 21:06:50 +0900

    Allocator Test Code Proof

--- mecab/allocator.py
+++ mecab/allocator.py
@@ -4,66 +4,99 @@ from mecab.utils.scoped_ptr import ScopedPtr, ScopedArray
 from mecab.utils.freelist import FreeList, ChunkFreeList
 
 class Allocator:
-    kResultsSize = 512
-    partial_buffer_ = []
+    __kResultsSize = 512
+    __partial_buffer_ = []
 
     def __init__(self, typeN: type, typeP: type):
-        self.type_n = typeN
-        self.type_p = typeP
+        self.__type_n = typeN
+        self.__type_p = typeP
 
         # member
-        self.id_ = 0
-        self.node_freelist_ = ScopedPtr(FreeList(type_=typeN, size=NODE_FREELIST_SIZE))
-        self.path_freelist_ = ScopedPtr(FreeList(type_=typeP, size=0))
-        self.char_freelist_ = ScopedPtr(ChunkFreeList(type_=str, size=0))
-        self.nbest_generator_ = ScopedPtr(NBestGenerator())
-        self.results_ = ScopedArray(type_=str, size=self.kResultsSize)
-        # self.results_ = ScopedArray()
+        self.__id_ = 0
+        self.__node_freelist_ = ScopedPtr(FreeList(type_=typeN, size=NODE_FREELIST_SIZE))
+        self.__path_freelist_ = ScopedPtr(FreeList(type_=typeP, size=0))
+        self.__char_freelist_ = ScopedPtr(ChunkFreeList(type_=str, size=0))
+        self.__nbest_generator_ = ScopedPtr(NBestGenerator())
+        self.__results_ = ScopedArray(type_=str, size=self.__kResultsSize)
+        # self.__results_ = ScopedArray()
 
     def newNode(self):
-        self.node_freelist_.data.alloc()
-        self.id_ = self.id_ + 1
-        return self.node_freelist_
+        """[summary]
+            add an element to node_freelist_
+            then increase id_
+        
+        Returns:
+            [typeN]: [description]
+        """
+        self.__node_freelist_.data.alloc()
+        self.__id_ = self.__id_ + 1
+        return self.__node_freelist_.data
 
     def newPath(self):
-        if self.path_freelist_.get() != None:
-            self.path_freelist_.reset(FreeList(type_=self.type_p, size=PATH_FREELIST_SIZE))
-        return self.path_freelist_.data.alloc()
+        """[summary]
+            add an element to path_freelist_
+        
+        Returns:
+            [typeP]: [description]
+        """
+        if self.__path_freelist_.get() != None:
+            self.__path_freelist_.reset(FreeList(type_=self.__type_p, size=PATH_FREELIST_SIZE))
+        self.__path_freelist_.data.alloc()
+        return self.__path_freelist_.data
 
-    def mutable_results(self):
-        return self.results_.get()
+    def mutable_results(self) -> list:
+        """[summary]
+        
+        Returns:
+            [list]: [description]
+        """
+        return self.__results_.get()
 
-    def alloc(self, size: int):
-        if self.char_freelist_.get() != None:
-            self.char_freelist_.reset(ChunkFreeList(type_=str, size=BUF_SIZE))
-        return self.char_freelist_.data.alloc(size + 1)
+    def alloc(self, size: int) -> list:
+        if self.__char_freelist_.get() != None:
+            self.__char_freelist_.reset(ChunkFreeList(type_=str, size=BUF_SIZE))
+        self.__char_freelist_.data.alloc(size + 1)
+        return self.__char_freelist_.data
     
     # python에서는 size의 의미가 필요한가 싶네요.
-    def strdup(self, _str: str, size: int) -> str:
-        n_str = _str
+    def strdup(self, str_: str, size: int) -> str:
+        n_str = str_
         return n_str
     
     def nbest_generator(self) -> NBestGenerator:
-        if self.nbest_generator_.get() != None:
-            self.nbest_generator_.reset(NBestGenerator())
-        return self.nbest_generator_.get()
+        if self.__nbest_generator_.get() != None:
+            self.__nbest_generator_.reset(NBestGenerator())
+        return self.__nbest_generator_.get()
     
-    def partial_buffer(self, size: int) -> int:
-        if len(self.partial_buffer_) == 0:
-            self.partial_buffer_ = ['' for _ in range(size)]
+    def partial_buffer(self, size: int) -> str:
+        if len(self.__partial_buffer_) == 0:
+            self.__partial_buffer_ = ['' for _ in range(size)]
         else:
-            self.partial_buffer_ = self.partial_buffer_[:(size + 1)]
-        return self.partial_buffer_[0]
+            self.__partial_buffer_ = self.__partial_buffer_[:(size + 1)]
+        return self.__partial_buffer_[0]
     
     def results_size(self) -> int:
-        return self.kResultsSize
+        return self.__kResultsSize
 
     def free(self):
-        self.id_ = 0
-        self.node_freelist_.data.free()
-        if self.path_freelist_.get() != None:
-            # self.path_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
-            self.path_freelist_.data.free()
-        if self.char_freelist_.get() != None:
-            # self.char_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
-            self.char_freelist_.data.free()
+        self.__id_ = 0
+        self.__node_freelist_.data.free()
+        if self.__path_freelist_.get() != None:
+            # self.__path_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
+            self.__path_freelist_.data.free()
+        if self.__char_freelist_.get() != None:
+            # self.__char_freelist_.get() 부분에 데이터가 제대로 확인 안됨.
+            self.__char_freelist_.data.free()
+    
+    # only debugging
+    def print_node_freelist(self):
+        print(self.__node_freelist_.data)
+    
+    def print_path_freelist(self):
+        print(self.__path_freelist_.data)
+    
+    def print_char_freelist(self):
+        print(self.__char_freelist_.data)
+    
+    def print_id(self):
+        print(self.__id_)
--- test/test_allocator.py
+++ test/test_allocator.py
@@ -6,30 +6,22 @@ class TestAllocator(unittest.TestCase):
 
     def test_generator(self):
         allocator = Allocator(int, int)
-        print("type_n: ", allocator.type_n)
-        print("type_p: ", allocator.type_p)
-        print("kResultsSize: ", allocator.kResultsSize)
-        print("partial_buffer_: ", allocator.partial_buffer_)
-        print("id_: ", allocator.id_)
-
-        print("node_freelist_: ", allocator.node_freelist_.data)
-        print("path_freelist_: ", allocator.path_freelist_.data)
-        print("char_freelist_: ", allocator.char_freelist_.data)
-        print("nbest_generator_: ", allocator.nbest_generator_.get())
-        print("results_: ", allocator.results_.get())
+        print("nbest_generator_: ", allocator.nbest_generator())
+        print("results_: ", allocator.mutable_results())
 
         allocator.newPath()
-        print("path_freelist_: ", allocator.path_freelist_.data)
+        print("path_freelist_: "), allocator.print_path_freelist()
         allocator.newNode()
-        print("id_: ", allocator.id_)
-        print("node_freelist_: ", allocator.node_freelist_.data)
+        print("id_: "), allocator.print_id()
+        print("node_freelist_: "), allocator.print_node_freelist()
+        
 
-        print("mutable_results(): ", allocator.mutable_results())
+        print("mutable_results(): "), allocator.mutable_results()
 
         print("alloc(): ", allocator.alloc(size=0))
-        print("char_freelist_: ", allocator.char_freelist_.data)
+        print("char_freelist_: "), allocator.print_char_freelist()
         print("alloc(): ", allocator.alloc(size=1))
-        print("char_freelist_: ", allocator.char_freelist_.data)
+        print("char_freelist_: "), allocator.print_char_freelist()
 
         print("strdup(): ", allocator.strdup("Test Allocator", size=10))
 
@@ -40,12 +32,12 @@ class TestAllocator(unittest.TestCase):
         print("results_size(): ", allocator.results_size())
 
         allocator.free()
-        print("id_: ", allocator.id_)
-        print("node_freelist_: ", allocator.node_freelist_.data)
+        print("id_: "), allocator.print_id()
+        print("node_freelist_: "), allocator.print_node_freelist()
         # self.path_freelist_.get() 부분이 제대로 동작 안하는 듯 합니다.
-        print("path_freelist_: ", allocator.path_freelist_.data)
+        print("node_freelist_: "), allocator.print_path_freelist()
         # self.char_freelist_.get() 부분이 제대로 동작 안하는 듯 합니다.
-        print("char_freelist_: ", allocator.char_freelist_.data)
+        print("char_freelist_: "), allocator.print_char_freelist()
 
 
 if __name__ == '__main__':
